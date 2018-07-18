8db61ee0b67b81d4fa89e39b0b064c2f5c101397
Sacha Roberts <sacha.roberts@ostendo.com>
2018-07-17 23:36:04 -0700

    first commit

---
+++ src/UnityAnimTranslator.h
@@ -0,0 +1,23 @@
+#ifndef UNITYANIMTRANSLATOR_H
+#define UNITYANIMTRANSLATOR_H
+
+#include <maya/MPxFileTranslator.h>
+
+class UnityAnimTranslator : public MPxFileTranslator
+{
+public:
+	UnityAnimTranslator();
+	virtual ~UnityAnimTranslator();
+	virtual MStatus writer(const MFileObject&, const MString&, MPxFileTranslator::FileAccessMode);
+	bool haveReadMethod() const { return false; }
+	bool haveWriteMethod() const { return true; }
+	MString defaultExtension() const { return MString(".anim"); }
+
+	
+
+};
+
+
+
+#endif // !UNITYANIMTRANSLATOR_H
+
